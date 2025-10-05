e5f76c58ef8e3911f1eac6a802e2fa08ac3092c9
Kollin <kollin@hey.com>
2025-10-05 00:49:52 -0500

    add derived tables to load routine

--- src/etl/load.py
+++ src/etl/load.py
@@ -2,11 +2,14 @@
 Top-level script to run the various ETLs populating the Simethicone database
 """
 
-from src.etl.annual_miles import run_annual_miles_etl
+from src.etl.annual_miles import run_annual_miles_etl  # noqa: I001
 from src.etl.fuel_efficiency import run_fuel_efficiency_etl
 from src.etl.vehicle_data import run_vehicle_data_etl
+from src.etl.derived_tables import create_derived_tables
 
 if __name__ == "__main__":
     run_annual_miles_etl()
-    run_fuel_efficiency_etl(use_local=False)
+    run_fuel_efficiency_etl(use_local=True)
     run_vehicle_data_etl()
+
+    create_derived_tables()
